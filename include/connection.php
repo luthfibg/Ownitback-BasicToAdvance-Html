a6fe27b5065a5a2a9b4864b51fba9329c3a5bbcb
luthfibg <luthfinewzoneonline@gmail.com>
2021-12-02 16:19:42 +0700

    v 0.0.3

--- include/connection.php
+++ include/connection.php
@@ -1,4 +1,14 @@
 <?php
-@$connect = mysqli_connect("localhost", "root", "") or die (mysqli_error());
-mysqli_select_db($connect, "dbownitback") or die (mysqli_error());
+
+@$server = "localhost";
+@$user = "root";
+@$pass = "";
+@$database = "dbownitback";
+@$connect = mysqli_connect($server, $user, $pass, $database);
+if(!$connect){
+	die("<script>alert('Database failed to load')</script>");
+} else {
+	echo "<script>alert('Success to load database')</script>";
+}
+
 ?>
